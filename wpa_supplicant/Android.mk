001b04fbb2273e79d28d4aa11d307b3da602ca3d
Vinay Gannevaram <quic_vganneva@quicinc.com>
2024-02-05 21:58:02 +0530

    Replace PMKSA cache inline stubs with wrapper function stubs
    PMKSA cache API is included in libpasn.so used by external modules,
    e.g., Wi-Fi Aware. To avoid dependency on IEEE8021X_EAPOL define for the
    external modules at compile time, remove PMKSA cache static inline
    functions from the header file and add wrapper function stubs.
    
    Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>
    
    CRs-Fixed: 3781338
    Git-repo: git://w1.fi/hostap.git
    Git-commit: ab37a57314256a6660bd4797c0f820cbd99133ea
    Change-Id: I3637e433913feeba212b9f939d1f4b7ccdf93037

--- wpa_supplicant/Android.mk
+++ wpa_supplicant/Android.mk
@@ -135,6 +135,7 @@ OBJS += src/utils/bitfield.c
 OBJS += src/utils/ip_addr.c
 OBJS += src/utils/crc32.c
 OBJS += src/common/ptksa_cache.c
+OBJS += src/rsn_supp/pmksa_cache.c
 OBJS += wmm_ac.c
 OBJS += op_classes.c
 OBJS += rrm.c
@@ -369,7 +370,6 @@ endif
 ifndef CONFIG_NO_WPA
 OBJS += src/rsn_supp/wpa.c
 OBJS += src/rsn_supp/preauth.c
-OBJS += src/rsn_supp/pmksa_cache.c
 OBJS += src/rsn_supp/wpa_ie.c
 OBJS += src/common/wpa_common.c
 NEED_AES=y
@@ -1820,8 +1820,9 @@ endif
 
 PASNOBJS += src/common/ptksa_cache.c
 
-ifndef CONFIG_NO_WPA
 PASNOBJS += src/rsn_supp/pmksa_cache.c
+
+ifndef CONFIG_NO_WPA
 PASNOBJS += src/rsn_supp/wpa_ie.c
 endif
 
