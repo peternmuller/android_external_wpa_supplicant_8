88f6df2752bf85cd6dd52fbe166b926976121c2a
Shivani Baranwal <quic_shivbara@quicinc.com>
2024-08-18 16:24:23 +0530

    P2P2: Update P2P Device Address when changing netdev address
    P2P2 adds support for privacy and needs ability to update the P2P Device
    Address. Update that whenever changing the interface address in
    wpa_supplicant for other (non-P2P) MAC address randomization purposes.
    
    Signed-off-by: Shivani Baranwal <quic_shivbara@quicinc.com>
    
    CRs-Fixed: 3911487
    Git-repo: git://w1.fi/srv/git/hostap.git
    Git-commit: 72e154dbd540fa68ef627a830762a5079bdeef6a
    Change-Id: Ida0d5c5ac4bdd0c6180c3d0e40652f8955d7af84

--- src/p2p/p2p.c
+++ src/p2p/p2p.c
@@ -3064,6 +3064,13 @@ bool is_p2p_dfs_chan_enabled(struct p2p_data *p2p)
 }
 
 
+void p2p_set_dev_addr(struct p2p_data *p2p, const u8 *addr)
+{
+	if (p2p && addr)
+		os_memcpy(p2p->cfg->dev_addr, addr, ETH_ALEN);
+}
+
+
 static void p2p_pairing_info_deinit(struct p2p_data *p2p)
 {
 #ifdef CONFIG_PASN
